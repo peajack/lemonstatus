237da62cc0580e200d33d60ce1ccfb9fde6a14fe
Peajack Boatan <peajackboatan@yandex.ru>
2024-10-21 19:59:44 +0700

    extend manpage

--- lemonstatus.h
+++ lemonstatus.h
@@ -3,7 +3,9 @@
 void die(char *call);
 static const char *xprop(char *name, char *type, Window win, long length);
 Window current_window(void);
+unsigned long *get_groups_num(void);
 unsigned long *get_group(void);
+static const char *get_groups_str(void);
 static const char *get_time(void);
 static const char *get_window_name(Window win);
 static const char *get_layout(void);
